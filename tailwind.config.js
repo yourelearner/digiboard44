bccbcf137827c19ba01ab3f5ce5d3aa2c020058e
Leonard <135197878+yourelearner@users.noreply.github.com>
2025-03-14 04:30:37 +0800

    Title update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        gold:'#FFD700',
+      },
+    },
   },
   plugins: [],
 };
